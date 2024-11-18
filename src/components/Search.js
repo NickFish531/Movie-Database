67e93822c045e887fdb33a8ddcb24150e704a662
Nick Fish <nickfish531@gmail.com>
2024-11-18 17:29:32 -0600

    Impliment

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-function Search () {
+function Search ({handleInput, search}) {
     return (
         <section className="searchbox-wrap">
-            <input type="text" placeholder='Seach for a movie...' className="searchbox" />
-
+            <input 
+                type="text" placeholder='Seach for a movie...' 
+                className="searchbox" 
+                onChange={handleInput} 
+                onKeyDown={search}
+            />
         </section>
     )
 }
